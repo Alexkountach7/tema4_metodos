4c66ad3fe2c933d5ddc935b4bd7c4780e5ffa917
A614\1DAW <alexkountach7@gmail.com>
2025-12-01 21:17:30 +0100

    ejercicios repasar

--- ejercicios_metodos/src/main/java/org/example/Main.java
+++ ejercicios_metodos/src/main/java/org/example/Main.java
@@ -21,7 +21,7 @@ public class Main {
         for (int i = 0; i < perros.length; i++) {
             System.out.println(perros[i]);
         }
-        varios("palabra", 1,2,3,4);
+
 
     }
     public static void varios (int... num) {
@@ -30,4 +30,25 @@ public class Main {
         }
 
     }
-}
+
+    public class ejercicio_2_menu {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("cuantas opciones quieres");
+            int n = sc.nextInt();
+            mostrarmenu(n);
+            System.out.println("elige una opcion");
+            int opcion = sc.nextInt();
+
+        }
+
+        public static void mostrarmenu(int n){
+            for (int i =1; i<n-1; i++){
+                System.out.println(i + ". Opcion" +i);
+            }
+            System.out.println(n +"salir");
+        }
+    }
+
+    }
+
